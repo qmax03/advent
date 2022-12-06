6472d9e56e801e37d0d1517579773a38f14d0e70
Jan Max Pavlica <janmax.pavlica@techfides.cz>
2022-12-06 06:28:01 +0100

    06

--- advent2022/advent06.js
+++ advent2022/advent06.js
@@ -1,7 +1,19 @@
-const {data, dataTest} = require('./adventXXData')
+const {data, dataTest, dataTest2} = require('./advent06Data')
 
-for(let i=0;)
 
+const dataProcess = data
+const uniqueChars = 14
+for (let i = 0; i < dataProcess.length; i++) {
+    const part = dataProcess.substring(i, i + uniqueChars)
+    const partPole = part.split('')
+    const hasDuplicates = partPole.some(item => partPole.filter(search => search === item).length > 1)
+    // console.log(hasDuplicates, i + 4)
+    if (!hasDuplicates) {
+        console.log(uniqueChars + i)
+        return
+    }
+
+}
 
 
 // console.log("first: ", sum1)
